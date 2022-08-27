c74435e3024187be7213613cf1b1f679cdfdbe8b
M V K <krishna.mugala9@gmail.com>
2022-08-27 21:12:52 +0530

    removed the set and get methods from class

--- src/main/java/com/niit/jdp/PetrolEngines.java
+++ src/main/java/com/niit/jdp/PetrolEngines.java
@@ -15,18 +15,5 @@ public class PetrolEngines {
     public PetrolEngines() {
     }
 
-    public PetrolEngines(String fuelType) {
-        this.fuelType = fuelType;
-    }
-
-    public String getFuelType() {
-        return fuelType;
-    }
-
-    public void setFuelType(String fuelType) {
-        this.fuelType = fuelType;
-
-    }
-
 
 }
