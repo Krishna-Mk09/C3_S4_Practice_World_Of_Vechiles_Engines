65696fa3f451220ce2ba1166a5d39e9f1fde4cce
M V K <krishna.mugala9@gmail.com>
2022-08-27 21:17:25 +0530

    added field in the class InternalCombustion

---
+++ src/main/java/com/niit/jdp/InternalCombustion.java
@@ -0,0 +1,14 @@
+/*
+ * Author Name : M.Krishna.
+ * Date: 27-08-2022
+ * Created With: IntelliJ IDEA Community Edition
+ *
+ */
+
+
+package com.niit.jdp;
+
+public class InternalCombustion {
+    private String fuelType;
+
+}
