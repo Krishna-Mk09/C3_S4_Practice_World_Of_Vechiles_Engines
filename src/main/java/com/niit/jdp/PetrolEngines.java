3e784c0fc8d74eb6a2b36a373bb54e68e0c6358f
M V K <krishna.mugala9@gmail.com>
2022-08-27 17:58:52 +0530

    added fuel type to the class petrol engines

---
+++ src/main/java/com/niit/jdp/PetrolEngines.java
@@ -0,0 +1,15 @@
+/*
+ * Author Name : M.Krishna.
+ * Date: 27-08-2022
+ * Created With: IntelliJ IDEA Community Edition
+ *
+ */
+
+
+package com.niit.jdp;
+
+public class PetrolEngines {
+
+    private String fuelType;
+
+}
