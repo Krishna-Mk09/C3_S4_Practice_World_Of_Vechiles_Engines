824f966aaa13401fffba8cba183da9fad7de847b
M V K <krishna.mugala9@gmail.com>
2022-08-27 17:48:48 +0530

    added getter and setter methods to the class

--- src/main/java/com/niit/jdp/Engine.java
+++ src/main/java/com/niit/jdp/Engine.java
@@ -18,6 +18,46 @@ public class Engine {
     public Engine() {
     }
 
+    public String getModelNumber() {
+        return modelNumber;
+    }
+
+    public void setModelNumber(String modelNumber) {
+        this.modelNumber = modelNumber;
+    }
+
+    public String getDisplacement() {
+        return displacement;
+    }
+
+    public void setDisplacement(String displacement) {
+        this.displacement = displacement;
+    }
+
+    public int getMaxPower() {
+        return maxPower;
+    }
+
+    public void setMaxPower(int maxPower) {
+        this.maxPower = maxPower;
+    }
+
+    public int getMaxRpm() {
+        return maxRpm;
+    }
+
+    public void setMaxRpm(int maxRpm) {
+        this.maxRpm = maxRpm;
+    }
+
+    public int getNumberOfCylinder() {
+        return NumberOfCylinder;
+    }
+
+    public void setNumberOfCylinder(int numberOfCylinder) {
+        NumberOfCylinder = numberOfCylinder;
+    }
+
     public Engine(String modelNumber, String displacement, int maxPower, int maxRpm, int numberOfCylinder) {
         this.modelNumber = modelNumber;
         this.displacement = displacement;
